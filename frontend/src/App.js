b3ab41bf1d5cd79ad0b8b2b06b357ae32ff693d6
AquasaAziz247 <azizaquasa@gmail.com>
2024-03-16 16:12:04 +0530

    Fronted Initialised

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+
+import './App.css';
+import React from 'react';
+import Header from './component/Header';
+
+
+const App = () => {
+  return (
+    <div className="bg-red-400">
+      ss
+      <Header />
+      
+      {/* Main content goes here */}
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/component/Header.js
@@ -0,0 +1,15 @@
+// Header.js
+
+import React from 'react';
+import logo from './src/assest/logo'
+
+const Header = () => {
+  return (
+  <div>
+    
+
+  </div>
+  );
+};
+
+export default Header;
